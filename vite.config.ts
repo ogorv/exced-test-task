ee5292d7c09432e399679bb380d61734cd1ce378
Ogor Vitali <vitaly.ogor@gmail.com>
2025-11-27 10:50:51 +0200

    Removed tailwind and installed boostrap

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { wayfinder } from '@laravel/vite-plugin-wayfinder';
-import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
@@ -11,7 +10,6 @@ export default defineConfig({
             ssr: 'resources/js/ssr.ts',
             refresh: true,
         }),
-        tailwindcss(),
         vue({
             template: {
                 transformAssetUrls: {
@@ -24,4 +22,12 @@ export default defineConfig({
             formVariants: true,
         }),
     ],
+    // Optional: Silence Sass deprecation warnings. See note below.
+    css: {
+        preprocessorOptions: {
+            scss: {
+                silenceDeprecations: ['import', 'mixed-decls', 'color-functions', 'global-builtin'],
+            },
+        },
+    },
 });
